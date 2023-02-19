83addc0860c78a75fcb5bcdfcabf5a5d5ad15a2d
Mikhail Vasilev <mgp-nsk@yandex.ru>
2023-02-19 21:28:18 +0700

    first release

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="fw-bold">Игра Five-On</div>
+    <div id="statusBar" :class="{ 'bg-success': gameStatus === 'Победа!' }">
+      <div class="d-flex justify-content-between">
+        <div>Состояние игры</div>
+        <div>{{ gameStatus }}</div>
+      </div>
+      <div class="d-flex justify-content-between">
+        <div>Сделано ходов</div>
+        <div>{{ steps }}</div>
+      </div>
+    </div>
+    <button id="restartButton" class="btn btn-warning" @click="initGame">
+      Старт
+    </button>
+    <div id="gameButtons">
+      <button
+        v-for="btnNumber of buttons"
+        :key="btnNumber"
+        class="btn btn-primary gm-button"
+        @click="clickGameButton(btnNumber)"
+      >
+        {{ btnNumber }}
+      </button>
+    </div>
+    <div id="gameLeds" class="d-flex">
+      <div
+        v-for="led of leds"
+        :key="led"
+        :class="['led', { 'bg-danger': ledsOn[led - 1] }]"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      gameStatus: "Неактивна",
+      steps: null,
+      buttons: 5,
+      buttonsTargets: [],
+      leds: 5,
+      ledsOn: [false, false, false, false, false],
+    };
+  },
+  methods: {
+    initGame() {
+      this.gameStatus = "Активна";
+      this.steps = 0;
+      this.buttonsTargets = [];
+      this.ledsOn = [false, false, false, false, false];
+      this.setLedsForButtons();
+    },
+    setLedsForButtons() {
+      for (let i = 0; i < this.buttons; i++) {
+        let additionalLeds = this.getRandomAdditionalLeds();
+        let oneButtonLeds = this.setLedsForOneButton(i, additionalLeds);
+        this.buttonsTargets.push(oneButtonLeds);
+      }
+      console.log(this.buttonsTargets);
+    },
+    getRandomAdditionalLeds() {
+      return Math.round(Math.random() * 3);
+    },
+    setLedsForOneButton(i, additionalLeds) {
+      let ledsForOneButton = [i + 1, 0, 0, 0];
+      for (let i = 1; i < additionalLeds + 1; ) {
+        let randomLedNumber = this.getRandomLedNumber();
+        let ledAlreadyInArr = this.checkLedNumberInLedsArray(
+          randomLedNumber,
+          ledsForOneButton
+        );
+        if (!ledAlreadyInArr) {
+          ledsForOneButton[i] = randomLedNumber;
+          i++;
+        }
+      }
+      return ledsForOneButton;
+    },
+    getRandomLedNumber() {
+      return Math.round(Math.random() * 4);
+    },
+    checkLedNumberInLedsArray(ledNumber, ledsArray) {
+      return ledsArray.includes(ledNumber);
+    },
+    clickGameButton(btnNumber) {
+      for (let i = 0; i < this.buttonsTargets[btnNumber - 1].length; i++) {
+        if (this.buttonsTargets[btnNumber - 1][i]) {
+          this.ledsOn[this.buttonsTargets[btnNumber - 1][i] - 1] =
+            !this.ledsOn[this.buttonsTargets[btnNumber - 1][i] - 1];
+        }
+      }
+      this.steps++;
+      this.checkWin();
+    },
+    checkWin() {
+      if (!this.ledsOn.includes(false)) {
+        this.gameStatus = "Победа!";
+      }
+    },
+  },
+
+  mounted() {
+    this.initGame();
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import "./five-on-styles.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
